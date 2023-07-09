0b9bf46537d9a18143df1c431b612987e7e980ce
haribo09876 <haribo09876@gmail.com>
2023-07-09 20:57:26 +0900

    20230709
    by KYW

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Page name'),
+      home: const MyHomePage(title: '                     Page name'),
     );
   }
 }
