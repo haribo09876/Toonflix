4cc50e1ca17ded9c4448b400799d1d01fee4467f
haribo09876 <haribo09876@gmail.com>
2023-07-10 14:13:42 +0900

    20230710
    by KYW

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+    return
+  }
+
 }
