06534811c49d14819b8fa769f3c6af9095dfc3a1
haribo09876 <haribo09876@gmail.com>
2023-07-23 14:31:34 +0900

    20230723
    by KYW

---
+++ lib/models/webtoon_episode_model.dart
@@ -0,0 +1,9 @@
+class WbetoonEpisodeModel {
+  final String id, title, rating, date;
+
+  WbetoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        rating = json['rating'],
+        date = json['date'];
+}
